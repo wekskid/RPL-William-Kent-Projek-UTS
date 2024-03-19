3de6476a1408ed45cf8ab2ecb9288ea655b53c7c
William Kent <147175184+wekskid@users.noreply.github.com>
2024-03-19 13:44:30 +0700

    Create Helper-William-Kent.js

---
+++ Helper-William-Kent.js
@@ -0,0 +1,13 @@
+// File: Helper-William-Kent.js
+
+class Helper {
+  static mergeStrings(string1, string2) {
+    return string1 + string2;
+  }
+
+  static splitStringByComma(string) {
+    return string.split(',');
+  }
+}
+
+module.exports = Helper;
